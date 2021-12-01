83f86b21db57ad421551374b3b5c07f9a0494d59
Sonne <sonne-34@yandex.ru>
2021-12-02 00:02:26 +0500

    01.12.21
    Выполнение домашнего задания используя циклы и коллекции.

--- app/src/main/java/com/example/testtgu/MainActivity.kt
+++ app/src/main/java/com/example/testtgu/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.testtgu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
     }
+
+    fun main() {
+        val myList = ('a'..'z').toMutableList()
+        myList.removeAll{ it.code % 2 != 0 }
+        for(i in myList){
+            println("$i ")
+        }
+    }
+
 }
